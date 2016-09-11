ed3ae7848fdd14c415a18ff7ea1f7987934e013d
Jakub Buriánek <buri.buster@gmail.com>
2016-09-11 09:02:56 +0200

    Format code with guidelines

--- src/Extension/ResourceExtension.php
+++ src/Extension/ResourceExtension.php
@@ -11,40 +11,44 @@ use Nette\DI\Statement;
 
 class ResourceExtension extends CompilerExtension
 {
-    public $defaults = [
-        'driver' => 'nettedb',
-        'definitions' => [],
-    ];
+	public $defaults = [
+		'driver' => 'nettedb',
+		'definitions' => [],
+	];
 
-    public $defaultResource = [
-        'presenter' => ResourcePresenter::class,
-    ];
+	public $defaultResource = [
+		'presenter' => ResourcePresenter::class,
+	];
 
-    public function loadConfiguration()
-    {
-        $config = $this->getNormalizedConfig();
-        $builder = $this->getContainerBuilder();
+	public function loadConfiguration()
+	{
+		$config = $this->getNormalizedConfig();
+		$builder = $this->getContainerBuilder();
 
-        // Load barebone definitions from service file
-        Compiler::loadDefinitions($builder, $this->loadFromFile(__DIR__ . '/../Configuration/services.neon'), $this->name);
+		// Load barebone definitions from service file
+		Compiler::loadDefinitions(
+			$builder,
+			$this->loadFromFile(__DIR__ . '/../Configuration/services.neon'),
+			$this->name
+		);
 
-        // Modify presenter factory to look for defined resource
-        $presenterFactory = $builder->getDefinition('application.presenterFactory');
-        $presenterFactory
-            ->setClass(PresenterFactory::class)
-            ->setFactory(PresenterFactory::class, [new Statement(
-                PresenterFactoryCallback::class, $presenterFactory->getFactory()->arguments[0]->arguments
-            )])
-            ->addSetup('setResourcesConfiguration', [$config]);
-    }
+		// Modify presenter factory to look for defined resource
+		$presenterFactory = $builder->getDefinition('application.presenterFactory');
+		$presenterFactory
+			->setClass(PresenterFactory::class)
+			->setFactory(PresenterFactory::class, [new Statement(
+				PresenterFactoryCallback::class, $presenterFactory->getFactory()->arguments[0]->arguments
+			)])
+			->addSetup('setResourcesConfiguration', [$config]);
+	}
 
-    private function getNormalizedConfig()
-    {
-        $config = $this->getConfig($this->defaults);
-        foreach ($config['definitions'] as $resource => $configuration) {
-            $config['definitions'][$resource] = array_merge($this->defaultResource, $configuration);
-        }
+	private function getNormalizedConfig()
+	{
+		$config = $this->getConfig($this->defaults);
+		foreach ($config['definitions'] as $resource => $configuration) {
+			$config['definitions'][$resource] = array_merge($this->defaultResource, $configuration);
+		}
 
-        return $config;
-    }
+		return $config;
+	}
 }
--- src/Presenter/PresenterFactory.php
+++ src/Presenter/PresenterFactory.php
@@ -6,35 +6,35 @@ use Nette\Application\PresenterFactory as BasePresenterFactory;
 
 class PresenterFactory extends BasePresenterFactory
 {
-    /** @var  array */
-    protected $resourcesConfiguration;
+	/** @var  array */
+	protected $resourcesConfiguration;
 
-    /**
-     * @param array $resourcesConfiguration
-     */
-    public function setResourcesConfiguration(array $resourcesConfiguration)
-    {
-        $this->resourcesConfiguration = $resourcesConfiguration;
-    }
+	/**
+	 * @param array $resourcesConfiguration
+	 */
+	public function setResourcesConfiguration(array $resourcesConfiguration)
+	{
+		$this->resourcesConfiguration = $resourcesConfiguration;
+	}
 
-    public function getPresenterClass(& $name)
-    {
-        foreach ($this->resourcesConfiguration['definitions'] as $resource => $configuration) {
-            $normalizedResourceName = $this->resourceToPresenter($resource);
-            if ($normalizedResourceName === $name) {
-                return $configuration['presenter'];
-            }
-        }
+	public function getPresenterClass(& $name)
+	{
+		foreach ($this->resourcesConfiguration['definitions'] as $resource => $configuration) {
+			$normalizedResourceName = $this->resourceToPresenter($resource);
+			if ($normalizedResourceName === $name) {
+				return $configuration['presenter'];
+			}
+		}
 
-        return parent::getPresenterClass($name);
-    }
+		return parent::getPresenterClass($name);
+	}
 
-    private function resourceToPresenter($name)
-    {
-        $formatted = mb_convert_case($name, MB_CASE_TITLE);
-        $formatted = preg_replace('/[-_]/', '', $formatted);
+	private function resourceToPresenter($name)
+	{
+		$formatted = mb_convert_case($name, MB_CASE_TITLE);
+		$formatted = preg_replace('/[-_]/', '', $formatted);
 
-        return $formatted;
-    }
+		return $formatted;
+	}
 
 }
