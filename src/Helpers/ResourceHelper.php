b5ce06864d1895a752876b6d030a5cdd13958b91
Jakub Buriánek <buri.buster@gmail.com>
2016-09-14 22:39:16 +0200

    Fix Nette 2.3 compatibility

--- src/Helpers/ResourceHelper.php
+++ src/Helpers/ResourceHelper.php
@@ -2,11 +2,26 @@
 
 namespace Buri\Resource\Helpers;
 
-use Nette\StaticClass;
+use Nette\Utils\ObjectMixin;
 
 class ResourceHelper
 {
-	use StaticClass;
+	/**
+	 * @throws \LogicException
+	 */
+	final public function __construct()
+	{
+		throw new \LogicException('Class ' . get_class($this) . ' is static and cannot be instantiated.');
+	}
+
+	/**
+	 * Call to undefined static method.
+	 * @throws MemberAccessException
+	 */
+	public static function __callStatic($name, $args)
+	{
+		ObjectMixin::strictStaticCall(get_called_class(), $name);
+	}
 
 	public static function normalizeResourceName($name)
 	{
